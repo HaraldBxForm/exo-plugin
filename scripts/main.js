d1402a34b8425b92434cd6c642bfb40ca1a8d26b
HaraldBxform <haraldbxform@gmail.com>
2025-06-11 08:32:34 +0200

    Init

--- scripts/main.js
+++ scripts/main.js
@@ -23,8 +23,9 @@ document.getElementById('notify').addEventListener('click', () => {
       position: "left",
       backgroundColor: "linear-gradient(0deg,rgba(34, 193, 195, 1) 0%, rgba(253, 187, 45, 1) 100%);",
     }).showToast();
-  });
+  }); 
   
+
 // ==============================
 // 🧲 Événements
 // ==============================
